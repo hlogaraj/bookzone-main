dfee17e130083eb46bb7140d43664085e06c3021
hlogaraj <hlogaraj@gmail.com>
2022-07-31 14:05:20 -0500

    f

---
+++ js/return.js
@@ -0,0 +1,33 @@
+var ordersTable = document.getElementById("ordered-items");
+var rows = ordersTable.childNodes();
+var returnItems = new Array();
+
+var products 
+
+for (let row in rows) {
+    let children = row.childNodes();
+    let checkbox = children[0];
+    let name = children[1];
+    if (checkbox.checked == "true") {
+        returnItems.push(products[name]);
+
+    }
+}
+console.log(returnItems);
+
+if (window.XMLHttpRequest) {
+    request = new XMLHttpRequest();
+} else {
+    request = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+request.open('GET', 'js/products.json');
+request.onreadystatechange = function () {
+    if ((request.status === 200) && (request.readyState === 4)) {
+        let json = JSON.parse(request.responseText);
+        products = json[0];
+        localStorage.setItem('products', JSON.stringify(products)); //save JSON string to local storage
+        console.log("Products loaded externally");
+        console.log(products);
+    }
+}
